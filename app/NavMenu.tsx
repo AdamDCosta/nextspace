0b02f1d454aa7d75f054c998ed139db1a959e2cd
AdamDCosta <adam.dcosta@googlemail.com>
2023-05-27 16:30:16 +0100

    refactored next auth file

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -7,7 +7,7 @@ export default function NavMenu() {
   return (
     <nav className="flex h-[70px] items-center justify-between bg-stone-950 p-2 text-stone-200">
       <Link href={"/"}>
-        <Image src="/logo.svg" width={216} height={30} alt="Nextspace logo" />
+        <Image src="/logo.svg" width={216} height={30} alt="Nextspace logo" priority={true}/>
       </Link>
       <ul className="mx-1 flex list-none">
         {links.map((link) => (
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,6 @@
-import { NextAuthOptions } from "next-auth";
-import NextAuth from "next-auth/next";
-import GithubProvider from 'next-auth/providers/github';
+import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth";
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ]
-};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
